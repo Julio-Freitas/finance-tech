f04419460d0da1ff81d30cf8249cbae991cd0cdd
julio.freitas <julio.freitas@solides.com.br>
2023-12-13 09:04:16 -0300

    feat: implements ci build

--- vite.config.ts
+++ vite.config.ts
@@ -8,11 +8,11 @@ import { resolve } from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  plugins: [react()],
   resolve: {
     alias: {
       'src/': resolve(__dirname, 'src') + '/',
     },
   },
-
-  plugins: [react()],
+  base: '/finance-tech/',
 });
