882ae437fbef0e1aea43a3918a0eca0eef05b820
julio.freitas <julio.freitas@solides.com.br>
2023-12-13 09:38:48 -0300

    feat: implements ci deploy

--- src/components/Sidenav/index.tsx
+++ src/components/Sidenav/index.tsx
@@ -1,16 +1,26 @@
-import { NavLink } from 'react-router-dom';
+import { NavLink, useLocation } from 'react-router-dom';
 import vendas from 'src/assets/icons/vendas.svg';
 import resumo from 'src/assets/icons/resumo.svg';
+
 import FintechSVG from '../Fintech';
 
+const classLi =
+  'cursor-pointer ransition ease-in-out duration-500 flex gap-6 hover:bg-color-2 px-8 py-2 hover:text-color-4 rounded items-center text-2xl';
+
 export function Sidenav() {
+  const { pathname } = useLocation();
+  pathname.replace(/\//gi, '');
+
   return (
     <nav className="box bg-3 gap-gap mr-6  !p-0 h-screen bg-color-3 w-72">
       <div className="p-8">
         <FintechSVG title="Fintech Logo" />
       </div>
       <ul className="flex flex-col pt-2 gap-4">
-        <li className="cursor-pointer ransition ease-in-out duration-500 flex gap-6 hover:bg-color-2 px-8 py-2 hover:text-color-4 rounded items-center text-2xl">
+        <li
+          className={`${classLi} data-[active="true"]:bg-color-2 data-[active="true"]:text-color-4`}
+          data-active={pathname.replace(/\//gi, '') === 'finance-tech'}
+        >
           <span>
             <img
               src={resumo}
@@ -19,11 +29,14 @@ export function Sidenav() {
               title="imagem logo"
             />
           </span>
-          <NavLink to="/">
+          <NavLink to="/finance-tech">
             <b>Resumo</b>
           </NavLink>
         </li>
-        <li className="cursor-pointer ransition ease-in-out duration-500 flex gap-6 hover:bg-color-2  px-8 py-2 hover:text-color-4 rounded items-center text-2xl">
+        <li
+          className={`${classLi} data-[active="true"]:bg-color-2 data-[active="true"]:text-color-4`}
+          data-active={pathname.replace(/\//gi, '') === 'vendas'}
+        >
           <span>
             <img
               src={vendas}
